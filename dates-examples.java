9f85aa3812a4383bae26e24161dea72f3221f079
Mauro Cicolella <mcicolella@libero.it>
2019-07-21 17:39:02 +0200

    Update dates-examples.java

--- dates-examples.java
+++ dates-examples.java
@@ -1,6 +1,8 @@
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+
 
 public class MyClass {
     public static void main(String args[]) {
@@ -8,5 +10,10 @@ public class MyClass {
         System.out.println("Instant: " + instant);
         LocalDateTime ldt = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
         System.out.println("Day: " + ldt.getDayOfWeek());
+        
+        Instant start = Instant.parse("2019-07-21T15:24:57.239Z");  
+        Instant end = Instant.parse("2019-07-21T15:35:19.385Z");  
+        System.out.println(ChronoUnit.MINUTES.between(start, end));
+
     }
 }
